ee6ee5a39a2dd1a79890d28f9460cd115481db8a
danirz <danirzdanirz@gmail.com>
2022-01-17 22:25:02 +0100

    danirzdanirz@gmail.com

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,10 @@ public class UserController {
 	@PostMapping("/save")
 	public void save(@RequestBody User users) {
 		userRepository.save(users);
+	}
+	
+	@GetMapping("/all")
+	public List<User> load(){
+		return userRepository.findAll();
 	}
 }
